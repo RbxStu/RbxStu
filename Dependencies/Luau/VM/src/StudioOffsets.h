c0a68929adae91eedde2fd218eba070dfe8533fb
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-04-18 20:20:11 -0300

    Update for `version-1870963560174427`

--- Dependencies/Luau/VM/src/StudioOffsets.h
+++ Dependencies/Luau/VM/src/StudioOffsets.h
@@ -9,70 +9,59 @@
 #define RebaseAddress(x) x - 0x140000000 + reinterpret_cast<uintptr_t>(GetModuleHandleA("RobloxStudioBeta.exe"))
 
 namespace RBX::Studio::Offsets {
-    const static std::uintptr_t luau_execute = RebaseAddress(0x142f80bf0);          // Search for "C Stack Overflow"
-    //const static std::uintptr_t luau_load = RebaseAddress(0x142f697a0);             // Search for "Bytecode Version"
+    const static std::uintptr_t luau_execute = RebaseAddress(0x142ffc270);          // Search for "C Stack Overflow"
 
     const static std::uintptr_t _luaO_nilobject = RebaseAddress(
-                                                          0x146603268);             // Get to lua_pushvalue and get inside of pseudo2addr, you will find the data xref.
+                                                          0x1466c5748);             // Get to lua_pushvalue and get inside of pseudo2addr, you will find the data xref.
     const static std::uintptr_t _luaH_dummynode = RebaseAddress(
-                                                          0x146603128);             // Find " ,"metatable": ", and go to the top of the second xref, a ldebug function. It will contain two references to luaH_dummynode.
+                                                          0x1466c5608);             // Find " ,"metatable": ", and go to the top of the second xref, a ldebug function. It will contain two references to luaH_dummynode.
 
     // The way we get our lua state is the classic, hooking.
-    const static std::uintptr_t pseudo2addr = RebaseAddress(0x142f74290);
+    const static std::uintptr_t pseudo2addr = RebaseAddress(0x142fef910);
 
-    const static std::uintptr_t rluaE_newthread = RebaseAddress(0x142f75780);
+    const static std::uintptr_t rluaE_newthread = RebaseAddress(0x142ff0e00);
     // Required for not crashing on errors.
     const static std::uintptr_t rLuaD_throw = RebaseAddress(
-                                                      0x142f78190);       // search for "memory allocation error: block too big". You will find luaG_runerrorL, last call is luaD_throw.
-    const static std::uintptr_t rlua_newthread = RebaseAddress(0x142f72200);
-    const static std::uintptr_t rTask_defer = RebaseAddress(0x141cb4d10);
+                                                      0x142ff3810);       // search for "memory allocation error: block too big". You will find luaG_runerrorL, last call is luaD_throw.
+    const static std::uintptr_t rlua_newthread = RebaseAddress(0x142fed880);
+    const static std::uintptr_t rTask_defer = RebaseAddress(0x141d0d280);
 
-    const static std::uintptr_t rFreeBlock = RebaseAddress(0x142f8ea70);
+    const static std::uintptr_t rFreeBlock = RebaseAddress(0x14300a0d0);
     const static std::uintptr_t rFromLuaState = RebaseAddress(
-                                                        0x141b05490);     // Appears to copy ones' L->userdata into another for new states. Search for "Failed to create Lua State", on the userthread user callback.
+                                                        0x141b57900);     // Appears to copy ones' L->userdata into another for new states. Search for "Failed to create Lua State", on the userthread user callback.
     const static std::uintptr_t rLuaD_rawrununprotected = RebaseAddress(
-                                                                  0x142f77f40);     // Search for luaD_rununprotected, crawl until you reach into luaD_pcall, from which, search for xrefs into a function referencing luaO_nilobject and pseudo2addr, it will also have an if check at the end checking for a negative value (first bit set).
+                                                                  0x142ff35c0);     // Search for luaD_rununprotected, crawl until you reach into luaD_pcall, from which, search for xrefs into a function referencing luaO_nilobject and pseudo2addr, it will also have an if check at the end checking for a negative value (first bit set).
 
-    const static std::uintptr_t rLuaC_Step = RebaseAddress(0x142f8c370);
+    const static std::uintptr_t rLuaC_Step = RebaseAddress(0x1430079d0);
 }
 
 struct lua_State;
 
 namespace FunctionTypes {
-    //using luau_load = int32_t (__fastcall *)(struct lua_State *L, const char *chunkName, const char *bytecode,
-    //                                         int32_t bytecodeSize,
-    //                                         int32_t env);
-    using luau_execute = void (__fastcall *)(struct lua_State *L);
+    using luau_execute = void (__fastcall *)(lua_State *L);
     using pseudo2addr = void *(__fastcall *)(lua_State *L, int32_t lua_index);
-    using lua_newthread = lua_State *(__fastcall *)(lua_State *L);
-    using rTask_spawn = int (__fastcall *)(lua_State *L);
     using rTask_defer = int (__fastcall *)(lua_State *L);
     using rluaE_newthread = lua_State *(__fastcall *)(lua_State *L);
     using rlua_newthread = lua_State *(__fastcall *)(lua_State *L);
-    using rbxAllocate = void *(__fastcall *)(std::uintptr_t size);
     using rFromLuaState = void (__fastcall *)(lua_State *LP, lua_State *L);
     using rFreeBlock = void (__fastcall *)(lua_State *L, int32_t sizeClass, void *block);
     using rLuaD_throw = void (__fastcall *)(lua_State *L, int32_t errcode);
-    using rLuaD_rawrununprotected = int32_t (__fastcall *)(struct lua_State *L,
-                                                           void (*PFunc)(struct lua_State *L, void *ud),
+    using rLuaD_rawrununprotected = int32_t (__fastcall *)(lua_State *L,
+                                                           void (*PFunc)(lua_State *L, void *ud),
                                                            void *ud);
     using rLuaC_Step = size_t (__fastcall *)(lua_State *L, bool assist);
 };
 
 namespace RBX::Studio::Functions {
-    // const static auto luau_load = reinterpret_cast<FunctionTypes::luau_load>(RBX::Studio::Offsets::luau_load);
     const static auto luau_execute = reinterpret_cast<FunctionTypes::luau_execute>(RBX::Studio::Offsets::luau_execute);
     const static auto rTask_defer = reinterpret_cast<FunctionTypes::rTask_defer>(RBX::Studio::Offsets::rTask_defer);
     const static auto rluaE_newthread = reinterpret_cast<FunctionTypes::rluaE_newthread>(RBX::Studio::Offsets::rluaE_newthread);
     const static auto rlua_newthread = reinterpret_cast<FunctionTypes::rlua_newthread>(RBX::Studio::Offsets::rlua_newthread);
-    // const static auto rbxAllocate = reinterpret_cast<FunctionTypes::rbxAllocate>(RBX::Studio::Offsets::rbxAllocate);
     const static auto rFromLuaState = reinterpret_cast<FunctionTypes::rFromLuaState>(RBX::Studio::Offsets::rFromLuaState);
     const static auto rFreeBlock = reinterpret_cast<FunctionTypes::rFreeBlock>(RBX::Studio::Offsets::rFreeBlock);
     const static auto rLuaD_throw = reinterpret_cast<FunctionTypes::rLuaD_throw>(RBX::Studio::Offsets::rLuaD_throw);
     const static auto rLuaD_rawrununprotected = reinterpret_cast<FunctionTypes::rLuaD_rawrununprotected>(RBX::Studio::Offsets::rLuaD_rawrununprotected);
     const static auto rLuaC_Step = reinterpret_cast<FunctionTypes::rLuaC_Step>(RBX::Studio::Offsets::rLuaC_Step);
-    //  We don't require of Robloxs' luaC_step
-    //  const static auto rLuaC_step = reinterpret_cast<FunctionTypes::rLuaC_step>(RBX::Studio::Offsets::rLuaC_step);
 }
 
 /*
