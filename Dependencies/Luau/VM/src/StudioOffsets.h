c7fe957d6bca2a678403b486a2a71174cb13e860
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-05-01 20:27:59 -0300

    Update for roblox studio `version-f0582db71b134926`

--- Dependencies/Luau/VM/src/StudioOffsets.h
+++ Dependencies/Luau/VM/src/StudioOffsets.h
@@ -11,52 +11,54 @@
 namespace RBX::Studio::Offsets {
     // Search for "C Stack Overflow"
     // Signature: 80 79 06 00 0F 85 ? ? ? ? E9 ? ? ? ?
-    const static std::uintptr_t luau_execute = RebaseAddress(0x14308c150);
+    const static std::uintptr_t luau_execute = RebaseAddress(0x1431c30b0);
 
     // Get to lua_pushvalue and get inside of pseudo2addr, you will find the data xref.
-    const static std::uintptr_t _luaO_nilobject = RebaseAddress(0x146758208);
+    const static std::uintptr_t _luaO_nilobject = RebaseAddress(0x1468e65c8);
 
     // Find " ,"metatable": ", and go to the top of the second xref, a ldebug
     // function. It will contain two references to luaH_dummynode.
-    const static std::uintptr_t _luaH_dummynode = RebaseAddress(0x1467580c8);
+    const static std::uintptr_t _luaH_dummynode = RebaseAddress(0x1468e6488);
 
     // The way we get our lua state is the classic, hooking.
     // Signature: 0F 84 88 00 00 00 81 FA EF D8 FF FF 74 45 81 FA F0 D8 FF FF 74 32 48 8B 41 ? 44 2B CA 48 8B 48 ? 4C 8B 01 41 0F B6 40 ? 44 3B C8 7F 12 41 8D 41 ? 48 98 48 83 C0 ? 48 C1 E0 ? 49 03 C0 C3
-    const static std::uintptr_t pseudo2addr = RebaseAddress(0x14307f7f0);
+    const static std::uintptr_t pseudo2addr = RebaseAddress(0x1431b6750);
 
     // search for coroutine.wrap. Inside there will be a lua_pushthread and lua_newthread call. Inside lua_newthread luaE_newthread is called.
-    const static std::uintptr_t rluaE_newthread = RebaseAddress(0x143080ce0);
+    const static std::uintptr_t rluaE_newthread = RebaseAddress(0x1431b7c40);
     // search for "memory allocation error: block too big". You will find luaG_runerrorL, last call is luaD_throw.
     // Signature for luaG_runerrorL (Calls luaD_throw): 48 89 50 ? 4C 89 40 ? 4C 89 48 ? 53 48 81 EC ? ? ? ? 48 8B D9 4C 8D 48 ? 4C 8B C2 48 8D 4C 24 ? BA ? ? ? ? E8 ? ? ? ? 48 8D 54 24 ? 48 8B CB E8 ? ? ? ? BA ? ? ? ? 48 8B CB E8 ? ? ? ?
-    const static std::uintptr_t rLuaD_throw = RebaseAddress(0x1430836f0);
+    const static std::uintptr_t rLuaD_throw = RebaseAddress(0x1431ba650);
     // search for coroutine.wrap. Inside there will be a lua_pushthread and lua_newthread call.
-    const static std::uintptr_t rlua_newthread = RebaseAddress(0x14307d760);
+    const static std::uintptr_t rlua_newthread = RebaseAddress(0x1431b46c0);
     // search for "defer"
-    const static std::uintptr_t rTask_defer = RebaseAddress(0x141d77ef0);
+    const static std::uintptr_t rTask_defer = RebaseAddress(0x141e2df80);
 
     // Get into luaC_step, then into gcstep and then into luaH_free, which inside has the call you need to freestack (or luaM_freearray I don't remember)
     // Which inside calls freeblock.
     // Signature: 4C 8B 51 ? 49 83 E8 ? 44 8B CA 4C 8B D9 49 8B 10 48 83 7A 28 00 75 22 83 7A 30 00 7D 1C 49 63 C1 49 8D 0C C2 49 8B 44 C2 ? 48 89 42 ? 48 85 C0 74 03 48 89 10 48 89 51 ? 48 8B 42 ? 49 89 00 83 6A ? ? 4C 89 42 ? 75 4B 48 8B 4A ? 48 85 C9 74 06 48 8B 02 48 89 01 48 8B 0A 48 85 C9 74 0A 48 8B 42 ? 48 89 41 ? EB 17 41 0F B6 C1 49 39 54 C2 60 49 8D 0C C2 75 08 48 8B 42 ? 48 89 41 ? 49 8B 43 ? 45 33 C9 4C 63 42 ? 48 8B 48 ? 48 FF 60 ?
-    const static std::uintptr_t rFreeBlock = RebaseAddress(0x14309a130);
+    const static std::uintptr_t rFreeBlock = RebaseAddress(0x1431d1200);
 
     // Appears to copy ones' L->userdata into another for new states. Search for
     // "Failed to create Lua State", on the userthread user callback.
-    const static std::uintptr_t rFromLuaState = RebaseAddress(0x141bbe4a0);
+    const static std::uintptr_t rFromLuaState = RebaseAddress(0x141c6b350);
 
     // Search for luaD_rununprotected, crawl until you reach into luaD_pcall, from which, search for xrefs into a function referencing
     // luaO_nilobject and pseudo2addr, it will also have an if check at the end checking for a negative value (first bit set).
-    const static std::uintptr_t rLuaD_rawrununprotected = RebaseAddress(0x1430834a0);
+    const static std::uintptr_t rLuaD_rawrununprotected = RebaseAddress(0x1431ba400);
 
-    const static std::uintptr_t rLuaC_Step = RebaseAddress(0x143097a30);
+    // Follow into lua_newthread, an if statement will lie there checking totalbytes and GCthreshold, that is luaC_needsgc, which is called inside luaC_checkgc, which calls luaC_step!
+    // Signature: 48 8B 59 ? B8 ? ? ? ? 0F B6 F2 0F 29 74 24 ? 4C 8B F1 44 8B 43 ? 44 0F AF 43 ? 48 8B 6B ? 48 2B 6B ? 41 F7 E8 8B FA C1 FF ? 8B C7 C1 E8 ? 03 F8 48 8B 83 ? ? ? ? 48 85 C0 74 04
+    const static std::uintptr_t rLuaC_Step = RebaseAddress(0x1431cead0);
 
     // search for "ProximityPrompt_Triggered".
-    const static std::uintptr_t fireproximityprompt = RebaseAddress(0x14252bfe0);
+    const static std::uintptr_t fireproximityprompt = RebaseAddress(0x14260fd70);
 
     // search for "InvalidInstance". Caller with two arguments (Modifies lua stack)
-    const static std::uintptr_t pushinstance = RebaseAddress(0x141c83de0);
+    const static std::uintptr_t pushinstance = RebaseAddress(0x141d2b770);
 
     // Search for "TaskSchedulerTargetFps". Use the .data segment reference it refers towards. It is a DWORD (or int32_t)
-    const static std::uintptr_t FFlag_TaskSchedulerTargetFps = RebaseAddress(0x148116844);
+    const static std::uintptr_t FFlag_TaskSchedulerTargetFps = RebaseAddress(0x14837cae4);
 } // namespace RBX::Studio::Offsets
 
 struct lua_State;
