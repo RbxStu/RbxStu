aa8d97cb3825598c028960515e23874bdd3dea80
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-05-08 19:06:08 -0300

    Fix filesystem library.

--- Environment/FilesystemLibrary.hpp
+++ Environment/FilesystemLibrary.hpp
@@ -13,12 +13,11 @@ class FilesystemLibrary final : public EnvironmentLibrary {
     static FilesystemLibrary *m_singleton;
     std::string location;
 
-protected:
-    ~FilesystemLibrary() = default;
-
 public:
+    ~FilesystemLibrary() = default;
     static FilesystemLibrary *get_singleton();
 
+    void set_workspace_path(const std::string &path) { this->location = path; }
     [[nodiscard]] std::string get_workspace_path() const { return this->location; }
 
     void register_environment(lua_State *L) override;
